01a9591eab65fff381e14a3ceef47b6c6740ca7d
AHMINA Marouan <AhminaMar1@gmail.com>
2020-10-15 19:36:17 +0100

    Add serach-service

--- gatsbyPart/src/pages/search.js
+++ gatsbyPart/src/pages/search.js
@@ -5,46 +5,48 @@ import RepairTokenAndCrawl from "../mainComponents/RepairTokenAndCrawl";
 import HelmetComponent from "../templates/HelmetComponent";
 import Header from "../templates/Header";
 import Footer from "../templates/Footer";
-import MainHome from "../mainComponents/MainHome";
+import MainSearch from "../mainComponents/MainSearch";
 
 
 
-export default function Home({data}) {
+
+export default function Search({data}) {
   const {allMarkdownRemark} = data;
 
   let colorTitlesStyle={
     color: '#FFF'
   }
+
+  
   return (
     <div>
-      <RepairTokenAndCrawl setToken={false} post_id="0" page_name="index"/>
-      <HelmetComponent title="المدونة"/>
-      <Header arrowActive="1" colorTitlesStyle={colorTitlesStyle} title1="مدونتي" title2="سأفتتحها لاحقاً.. شكراً على زيارتك"/>
-      <MainHome data={allMarkdownRemark.edges}/>
+      <RepairTokenAndCrawl setToken={false} post_id="0" page_name="search"/>
+      <HelmetComponent title="ركن البحث"/>
+      <Header arrowActive="1" colorTitlesStyle={colorTitlesStyle} title1="إبحث في المدونة" title2="سأفتتحها لاحقاً.. شكراً على زيارتك"/>
+      <MainSearch data={allMarkdownRemark.edges}/>
       <Footer />
     </div>)
 }
 
 
 export const pageQuery = graphql`
-query MyQueryIndex {
+query MyQuerySearch {
     allMarkdownRemark (
         sort: { order: DESC, fields: [frontmatter___id] }
         filter: { fileAbsolutePath: { regex: "//posts//" } }
-        limit: 10
     ){
       edges {
         node {
           frontmatter {
             id
-            slug
             title
+            slug
             date
-            author
             description
+            keyword
           }
         }
       }
     }
-  }
+  }  
 `
--- gatsbyPart/src/templates/SearchPostIcon.js
+++ gatsbyPart/src/templates/SearchPostIcon.js
@@ -7,7 +7,7 @@ const SearchPost = () => {
         <div className="frame_header_serch-icon">
             <div className="header_serch-icon_space-left"></div>
             <div className="header_serch-icon">
-                <Link to='/' className="header_serch-icon_for-center">
+                <Link to='/search' className="header_serch-icon_for-center">
                     <i className="fas fa-search"></i>
                 <span>Search</span>
 
