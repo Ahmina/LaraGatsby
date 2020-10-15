123561e37a9587ec0a6be3cfab1c8ebc33a616cf
AHMINA Marouan <AhminaMar1@gmail.com>
2020-10-15 01:44:38 +0100

    Init pages/portfolio.js to use the Json and photos inside portfolio-folder

--- gatsbyPart/src/pages/portfolio.js
+++ gatsbyPart/src/pages/portfolio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {graphql} from "gatsby";
 import "../style/main.css";
 import RepairTokenAndCrawl from "../mainComponents/RepairTokenAndCrawl";
 import HelmetComponent from "../components/HelmetComponent";
@@ -8,17 +9,38 @@ import Footer from "../templates/Footer";
 
 
 
-export default function Porftolio() {
-
+export default function Porftolio({data}) {
   let colorTitlesStyle={
     color: '#FFF'
   }
   return (
     <div>
-      <RepairTokenAndCrawl setToken={false} post_id="0" page_name="porftolio"/>
+      <RepairTokenAndCrawl setToken={false} post_id="0" page_name="portfolio"/>
       <HelmetComponent title="معرض الأعمال"/>
       <Header arrowActive="2" colorTitlesStyle={colorTitlesStyle} title1="الرحاب في معرض الأعمال" title2="معرض أعمال لرحلة تعلم وتطوير في الويب وتقنياته لأكثر من سبع سنوات"/>
-      <MainPortfolio/>
+      <MainPortfolio data={data.allPortfolioJson.edges}/>
       <Footer />
     </div>)
 }
+
+export const pageQuery2 = graphql`
+query MyQuery2{
+  allPortfolioJson(sort: {fields: id, order: DESC}) {
+    edges {
+      node {
+        id
+        title
+        link
+        description
+        date
+        star
+        table
+        pics{
+          publicURL
+        }
+      }
+    }
+  }
+}
+
+`
