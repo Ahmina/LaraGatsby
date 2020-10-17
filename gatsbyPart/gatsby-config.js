2a52522bb2525e1afbb28fe37f12e3871560e089
AHMINA Marouan <AhminaMar1@gmail.com>
2020-10-17 19:48:12 +0100

    Add RSS-service of posts (articles)

--- gatsbyPart/gatsby-config.js
+++ gatsbyPart/gatsby-config.js
@@ -47,5 +47,62 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.description,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark (
+                  sort: { order: DESC, fields: [frontmatter___id] }
+                  filter: { fileAbsolutePath: { regex: "//posts//" } }
+                ){
+                  edges {
+                    node {
+                      html
+                      frontmatter {
+                        id
+                        slug
+                        title
+                        date
+                        author
+                        keyword
+                        description
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "AhminaMar1 Blog RSS Feed",
+          },
+        ],
+      },
+    },
   ],
 }
